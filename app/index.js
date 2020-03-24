8587385e922fc32d47373ad44437511449754763
Lauryn Bassett <lauryn.bassett@gmail.com>
2020-03-24 11:28:12 -0500

    init commit

---
+++ app/index.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import store, {increment} from './store' // imported for you already
+
+class Counter extends React.Component {
+  constructor () {
+    super()
+    this.state = store.getState();
+    this.clickHandler = this.clickHandler.bind(this)
+  }
+
+  componentDidMount() {
+    this.functionToCallWhenWeWantToUnsubscribe = store.subscribe(() => {
+      this.setState(store.getState())
+    })
+  }
+
+  componentWillUnmount() {
+    this.functionToCallWhenWeWantToUnsubscribe();
+  }
+
+  clickHandler () {
+    const action = increment();
+    store.dispatch(action)
+  }
+
+  render () {
+    return (
+      <div id='container'>
+        <div id='counter'>
+          <h1>{this.state.count}</h1>
+          <button onClick={(this.clickHandler)}>Increment</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <Counter />,
+  document.getElementById('app')
+)
